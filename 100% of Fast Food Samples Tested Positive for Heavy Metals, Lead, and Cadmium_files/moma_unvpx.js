9d806e68326a6956ea2f544048d2bbe322729c0b
cynnamerica <147957125+cynnamerica@users.noreply.github.com>
2023-10-14 18:10:03 -0700

    Add files via upload

---
+++ 100% of Fast Food Samples Tested Positive for Heavy Metals, Lead, and Cadmium_files/moma_unvpx.js
@@ -0,0 +1,52 @@
+/**
+ *      ##############################################################################
+ *     ##  ______ ____ ______ __     ____  ______ ______ _____ ______  __     ___   ##
+ *    ##  / ____//  _// ____// /    / __ \/_  __// ____// ___//_  __/ / /    /   |  ##
+ *   ##  / /_    / / / __/  / /    / / / / / /  / __/   \__ \  / /   / /    / /| |  ##
+ *  ##  / __/  _/ / / /___ / /___ / /_/ / / /  / /___  ___/ / / /_  / /___ / ___ |  ##
+ * ##  /_/    /___//_____//_____//_____/ /_/  /_____/ /____/ /_/(_)/_____//_/  |_|  ##
+ * ###################################################################################
+ * ##  Crafted with pride in Los Angeles, California              (c) Fieldtest.la  ##
+ * ###################################################################################
+ *
+ * Copyright (c) 2014-2023, FieldTest, Inc. All rights reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Version : 2.3 : ad2f981820c1e26b9c95b61832af190a
+ */
+
+window.fieldtest = {"key":"MOMA","template":"moma_unvpx.js","value":null,"rhost":"www.momsacrossamerica.com","rpath":"\/","cnv":null,"adv":"9ceryud"};
+window.fieldtest.gs=function(e,a){t=!1,n=document.createElement("script"),d=document.getElementsByTagName("head")[0];n.src=e,n.async=!0,n.onload=n.onreadystatechange=function(){t||this.readyState&&"loaded"!==this.readyState&&"complete"!==this.readyState||(t=!0,a(),n.onload=n.onreadystatechange=null,d.removeChild(n))},d.appendChild(n)};
+window.fieldtest.pushe=function(e,n,a,d){t=n.getElementsByTagName(a)[0],"undefined"==typeof t&&(t=n.getElementsByTagName("head")[0],"undefined"==typeof t&&(t=n.getElementsByTagName("html")[0])),"undefined"!=typeof t&&t.appendChild(d)};
+
+!function(w,d,t,s,i){
+    if(w.ttdunvdiv){return;}else{w.ttdunvdiv=true;}
+    t=d.createElement('div');t.id='ttdUniversalPixelTag754596cc5d70dc54d06f33fe3c538939';t.style='display:none';
+    w.fieldtest.pushe(w,d,'body',t);
+}(window,document);
+
+!function(w,d){
+    if(!w.ftunvpx){
+        w.ftunvpx = true;
+        w.fieldtest.gs('https://js.adsrvr.org/up_loader.1.1.0.js', function(){
+            var s=d.createElement( 'script' );s.async=1,s.type='text/javascript';
+            s.innerHTML='(function(global) { \n'+
+                'if (typeof TTDUniversalPixelApi === \'function\') { \n'+
+                'var universalPixelApi = new TTDUniversalPixelApi();\n'+
+                'universalPixelApi.init("9ceryud",  ["juvf747"], "https://insight.adsrvr.org/track/up", "ttdUniversalPixelTag754596cc5d70dc54d06f33fe3c538939"); \n'+
+                '}})(this);\n';
+            var t=document.getElementById('ttdUniversalPixelTag754596cc5d70dc54d06f33fe3c538939');t.appendChild(s);
+        });
+    }
+}(window,document);
+
+
+
+// Cached: Pixel-tag:MOMA/moma_unvpx.js
+
